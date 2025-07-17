aec940e9b2edca3cda04a80a97a0a10202bb9ea3
Gabe Jackson <gabejackson01@gmail.com>
2025-07-17 15:30:41 -0400

    Changed resume and added download button

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -6,9 +6,14 @@ export default function Resume(){
     return(
         <>
         <MyHeader/>
-        <div className={styles.resume}>
-            <img className = {styles.image} src='resume-7.pdf' />
+        <div className={styles.download}>
+            <a href="/Resume-8.pdf" download="Gabriel-Jackson-Resume.pdf">Download</a>
         </div>
+
+        <div className={styles.resume}>
+            <img className = {styles.image} src='resume-8.pdf' />
+        </div>
+
         </>
     )
 
