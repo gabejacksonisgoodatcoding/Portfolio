b2bab0d94b7bbe223bb9da0caf97e928ee28c546
Gabe <gabejackson01@gmail.com>
2025-08-19 13:16:07 -0400

    Finished Tic Tac Toe

---
+++ app/Blackjack/Blackjack.jsx
@@ -0,0 +1,237 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import styles from './Blackjack.module.css';
+
+import {createPngMap, Deck, dealPlayer, cardAdd} from '@/app/Blackjack/cardToPNG';
+import MyHeader from '../components/myHeader/myHeader';
+
+
+
+export default function Blackjack({winnerFunc, resetButton}){
+
+    const [myDeck, setMyDeck] = useState(() =>{
+        return new Deck();
+    });
+
+    const[playerHand, setPlayerhand] = useState([]);
+    const [dealerHand, setDealerHand] = useState([]);
+
+    const[displayCards, setDisplayCards] = useState([]);
+    const[displayDealerCards, setDisplayDealerCards] = useState([]);
+
+    const[score, setScore] = useState(() =>{
+        return cardAdd(playerHand);
+    });
+     const[dealerScore, setDealerScore] = useState(() =>{
+        return cardAdd(dealerHand);
+    });
+
+    const[buttonDisabled, setButtonDisabled] = useState(false)
+    const [dealerHiddenCard, setDealerHiddenCard] = useState(null);
+    const mymap = createPngMap(myDeck);
+    const [dealerDraw, setDealerDraw] = useState(false);
+    const [playerEndTurn, setPlayerEndTurn] = useState(false);
+    const [dealerEndTurn, setDealerEndTurn] = useState(false);
+    const [gameOver, setGameOver] = useState(false);
+    
+    
+
+
+
+    useEffect(()=>{
+        myDeck.shuffle();
+    },[]);
+
+    useEffect(()=>{
+        if(!dealerDraw){
+            return
+        }
+        if(score !== 21 && dealerScore < 17 && dealerScore !== 21) {
+            setTimeout(() => {
+            dealerDrawCard(); 
+            }, 500);
+        }else{
+            setDealerDraw(false);
+            setDealerEndTurn(true);
+        }
+
+    },[dealerDraw, dealerScore]);
+
+    useEffect(() =>{
+        CheckWinner();
+    }, [score, dealerScore, dealerEndTurn])
+
+
+    
+
+
+    function playerTurnStart(){
+       let newHand = dealPlayer(myDeck);
+       console.log(newHand[0] + newHand[1]);
+       setPlayerhand(newHand);
+       for(const card of newHand){
+            setDisplayCards(prev => [...prev, mymap.get(card)]);
+       }
+       setScore(cardAdd(newHand));
+       setButtonDisabled(true)
+    }
+
+    function drawCard(){
+        let newCard = myDeck.drawCard();
+        //console.log(newCard);
+        let newHand = [...playerHand.slice(0, playerHand.length + 1), newCard];
+        setPlayerhand(newHand);
+        setDisplayCards(prev => [...prev, mymap.get(newCard)]);
+        console.log("Player Hand: "+ playerHand);
+        setScore(cardAdd(newHand));
+    
+    }
+
+    function dealerDrawCard(){
+        let newCard = myDeck.drawCard();
+        //console.log(newCard);
+        let newHand = [...dealerHand.slice(0, dealerHand.length + 1), newCard];
+        setDealerHand(newHand);
+        setDisplayDealerCards(prev => [...prev, mymap.get(newCard)]);
+        setDealerScore(cardAdd(newHand));
+        
+    
+    }
+
+    function displayHand(){
+        let imageArray = []
+        for(const png of displayCards){
+            imageArray.push(<img key={png} src={`/CardPNGs/${png}`}></img>);
+        }
+        return imageArray
+    }
+    function playerTurnStart(){
+        let newHand = dealPlayer(myDeck);
+        console.log(newHand[0] + newHand[1]);
+        setPlayerhand(newHand);
+        for(const card of newHand){
+                setDisplayCards(prev => [...prev, mymap.get(card)]);
+        }
+        setScore(cardAdd(newHand));
+
+        newHand = dealPlayer(myDeck);
+        let dealerShownCard = newHand[0]
+        setDealerHiddenCard(mymap.get(newHand[1])); 
+        let dealerDisplayArray = [];
+
+        dealerDisplayArray.push(mymap.get(dealerShownCard))
+        dealerDisplayArray.push("card back red.png")
+
+        setDealerHand(newHand);
+        setDisplayDealerCards(dealerDisplayArray);
+        
+        setDealerScore(cardAdd([dealerShownCard]));
+        setButtonDisabled(true);
+    }
+
+    function dealerTurnStart(){
+        setPlayerEndTurn(true);
+        let newDealerDisplayArray = displayDealerCards.slice(0, displayDealerCards.length + 1);
+        
+        newDealerDisplayArray[1] = dealerHiddenCard;
+
+        console.log("New Dealer Display Array[1] = "+ newDealerDisplayArray[1])
+        setDisplayDealerCards(newDealerDisplayArray);
+        setDealerScore(cardAdd(dealerHand));
+        setDealerDraw(true);
+    
+    }
+
+
+    function setWinnerFunc(winnerNum){
+        let playerWins = "Player Wins!"
+        let dealerWins = "Dealer Wins :("
+        let tie = "A tie happens, push"
+        switch(winnerNum){
+            case 0:
+                return
+            case 1:
+                setGameOver(true)
+                winnerFunc(playerWins)
+                break;
+            case 2:
+                setGameOver(true)
+                winnerFunc(dealerWins)
+                break;
+            case 3:
+                setGameOver(true)
+                winnerFunc(tie)
+                break;
+        }
+    }
+    function CheckWinner(){
+        let playerWins = 1
+        let dealerWins = 2
+        let tie = 3
+        let result = 0;
+
+
+        if(score > 21){ // player busts
+            result = dealerWins
+        }
+        if(playerEndTurn && dealerEndTurn){
+            if(score === 21 && dealerScore !== 21){ //player 21
+            result = playerWins;
+            }
+            if(dealerScore == 21 && score !== 21){  // dealer 21
+                result = dealerWins
+            }
+
+            if(score < 21 && dealerScore > 21){ //dealer busts
+                result = playerWins
+            }
+            if(score > dealerScore && score < 21){  //player greater score
+                result = playerWins
+            }
+            if(dealerScore > score && dealerScore < 21){ // dealer score greater
+                result = dealerWins
+            }
+            if(dealerScore == score){
+                result = tie
+            }
+        }
+        console.log(result)
+        setWinnerFunc(result);
+
+    }
+    function displayDealer(){
+        let imageArray = []
+        for(const png of displayDealerCards){
+            imageArray.push(<img key={png} src={`/CardPNGs/${png}`}></img>);
+        }
+        return imageArray
+    }
+
+
+    return(
+        <>
+
+        <div className={styles.Game}> 
+            <button disabled = {buttonDisabled} onClick={playerTurnStart}>Start game</button>
+            <br></br>
+            <button disabled = {!buttonDisabled || gameOver}onClick={drawCard}>Hit</button>
+            <br></br>
+            <button disabled = {!buttonDisabled || gameOver} onClick={dealerTurnStart}>Stand</button>
+            <br></br>
+            {resetButton()}
+            {buttonDisabled && <p>Your Cards:</p>}
+            {buttonDisabled && <p>Your Score: {score}</p>}
+            <div className={styles.displayCards}>
+                {displayHand()}
+            </div>
+            {buttonDisabled && <p>Dealer Cards:</p>}
+            {buttonDisabled && <p>Dealer Score: {dealerScore}</p>}
+             <div className={styles.displayCards}>
+                {displayDealer()}
+            </div>
+            
+        </div>
+
+        </>
+    );
+};
--- app/Blackjack/page.jsx
+++ app/Blackjack/page.jsx
@@ -1,167 +1,53 @@
 'use client'
-import React, { use, useEffect, useState } from 'react';
-import MyHeader from '@/app/components/myHeader/myHeader';
-import styles from './Blackjack.module.css';
+import React, { useEffect, useState } from 'react';
 
-import {createPngMap, Card, Deck, dealPlayer, cardAdd} from '@/app/Blackjack/cardToPNG';
-
-
-
-export default function Blackjack(){
-
-    const [myDeck, setMyDeck] = useState(() =>{
-        return new Deck();
-    });
-
-    const[playerHand, setPlayerhand] = useState([]);
-    const [dealerHand, setDealerHand] = useState([]);
+import Blackjack from '@/app/Blackjack/Blackjack'
+import styles from './Blackjack.module.css'
+import MyHeader from '../components/myHeader/myHeader';
 
 
 
-    const[displayCards, setDisplayCards] = useState([]);
-    const[displayDealerCards, setDisplayDealerCards] = useState([]);
+export default function Game(){
+    const [reset, setReset] = useState(true);
+    const [winner, setWinner] = useState("")
+    const [resetAllow, setResetAllow] = useState(false);
 
-    const[score, setScore] = useState(() =>{
-        return cardAdd(playerHand);
-    });
-     const[dealerScore, setDealerScore] = useState(() =>{
-        return cardAdd(dealerHand);
-    });
-
-    const[buttonDisabled, setButtonDisabled] = useState(false);
-    const[dealerTurn, setDealerTurn] = useState(false);
-    const mymap = createPngMap(myDeck);
-
-    const [dealerHiddenCard, setDealerHiddenCard] = useState(null);
-
-
-
-
-    useEffect(()=>{
-        myDeck.shuffle();
-        console.log("Im usingEffect");
-    },[]);
-
-    useEffect(()=>{
-        if(!dealerTurn){
+    useEffect(() =>{
+        if(!reset){
+            setReset(true);
+        }else{
             return
         }
-        if (score !== 21 && dealerScore < 17 && dealerScore !== 21) {
-            setTimeout(() => {
-            dealerDrawCard(); // This updates dealerScore
-            }, 500);
-        } else {
-            setDealerTurn(false); // Stop drawing if done
-  }},[dealerScore, dealerTurn]);
+    },[reset])
 
-    function playerTurnStart(){
-       let newHand = dealPlayer(myDeck);
-       console.log(newHand[0] + newHand[1]);
-       setPlayerhand(newHand);
-       for(const card of newHand){
-            setDisplayCards(prev => [...prev, mymap.get(card)]);
-       }
-       setScore(cardAdd(newHand));
-       setButtonDisabled(true)
-    }
-
-    function drawCard(){
-        let newCard = myDeck.drawCard();
-        //console.log(newCard);
-        let newHand = [...playerHand.slice(0, playerHand.length + 1), newCard];
-        setPlayerhand(newHand);
-        setDisplayCards(prev => [...prev, mymap.get(newCard)]);
-        console.log("Player Hand: "+ playerHand);
-        setScore(cardAdd(newHand));
-
-    }
-
-    function dealerDrawCard(){
-    let newCard = myDeck.drawCard();
-    //console.log(newCard);
-    let newHand = [...dealerHand.slice(0, dealerHand.length + 1), newCard];
-    setDealerHand(newHand);
-    setDisplayDealerCards(prev => [...prev, mymap.get(newCard)]);
-    setDealerScore(cardAdd(newHand));
-    
-    }
-
-    function displayHand(){
-        let imageArray = []
-        for(const png of displayCards){
-            imageArray.push(<img key={png} src={`/CardPNGs/${png}`}></img>);
+    function gameSetWinner(winnerStr){
+        setWinner(winnerStr);
+        if(winnerStr !== ""){
+            setResetAllow(true);
         }
-        return imageArray
-    }
-    function playerTurnStart(){
-        let newHand = dealPlayer(myDeck);
-        console.log(newHand[0] + newHand[1]);
-        setPlayerhand(newHand);
-        for(const card of newHand){
-                setDisplayCards(prev => [...prev, mymap.get(card)]);
-        }
-        setScore(cardAdd(newHand));
-
-        newHand = dealPlayer(myDeck);
-        let dealerShownCard = newHand[0]
-        setDealerHiddenCard(mymap.get(newHand[1])); 
-        let dealerDisplayArray = [];
-
-        dealerDisplayArray.push(mymap.get(dealerShownCard))
-        dealerDisplayArray.push("card back red.png")
-
-        setDealerHand(newHand);
-        setDisplayDealerCards(dealerDisplayArray);
-        
-        setDealerScore(cardAdd([dealerShownCard]));
-        setButtonDisabled(true);
     }
 
-    function dealerTurnStart(){
-        let newDealerDisplayArray = displayDealerCards.slice(0, displayDealerCards.length + 1);
-        
-        newDealerDisplayArray[1] = dealerHiddenCard;
-
-        console.log("New Dealer Display Array[1] = "+ newDealerDisplayArray[1])
-        setDisplayDealerCards(newDealerDisplayArray);
-        setDealerScore(cardAdd(dealerHand));
-
-        setDealerTurn(true);
-        
+    function resetGame(){
+        setWinner("")
+        console.log("Hello ")
+        setReset(false);
+        setResetAllow(false);
     }
 
-    function displayDealer(){
-        let imageArray = []
-        for(const png of displayDealerCards){
-            imageArray.push(<img key={png} src={`/CardPNGs/${png}`}></img>);
-        }
-        return imageArray
+    function resetButton(){
+        return (<button  disabled = {!resetAllow} className= {styles.myButton}onClick={resetGame}>Play Again</button>);
     }
-
 
     return(
         <>
         <MyHeader>
         </MyHeader>
-        <div className={styles.Game}>
-            <button disabled = {buttonDisabled} onClick={playerTurnStart}>Start game</button>
-            <button disabled = {!buttonDisabled}onClick={drawCard}>draw cards</button>
-            <button disabled = {!buttonDisabled}onClick={dealerTurnStart}>End Turn</button>
-            <p>Your Cards:</p>
-            <p>Your Score: {score}</p>
-            <div className={styles.displayCards}>
-                {displayHand()}
-            </div>
-            <p>Dealer Cards:</p>
-            <p>Dealer Score: {dealerScore}</p>
-             <div className={styles.displayCards}>
-                {displayDealer()}
-            </div>
+        <div className={styles.background}>
 
+        {reset && <Blackjack winnerFunc = {gameSetWinner} resetButton = {resetButton}/>}
+        {winner}
+        <br></br>
         </div>
-
-
-
         </>
     );
 };
