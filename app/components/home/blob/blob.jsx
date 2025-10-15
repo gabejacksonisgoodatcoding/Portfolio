02bda8e2b88345a57113294a4c69c9af2286ad51
Gabe Jackson <gabejackson01@gmail.com>
2025-10-15 17:14:16 -0400

    fixes to home page and header to make mobile viewing better

--- app/components/home/blob/blob.jsx
+++ app/components/home/blob/blob.jsx
@@ -4,7 +4,7 @@ export default function Blob({blobTitle, blobBody}){
     return(
         <div>
             <div className="text-4xl text-center px-4">
-            <p>{blobTitle}</p>
+            <p className="whitespace-pre-line">{blobTitle}</p>
             </div>
             <div className="text-2xl text-left px-4">
             <p>{blobBody}</p>
--- app/page.jsx
+++ app/page.jsx
@@ -19,50 +19,39 @@ import Blob  from '@/app/components/home/blob/blob'
 
 export default function Home() {
 
-  const listItems = skills.map(skill => 
-
-    <li
-        key={skill.id}
-        >
-        {skill.title}
-        </li>
-  );
   
   return (
-  <>
+  <div className='flex overflow-x-hidden min-h-screen flex-col'>
   <MyHeader />
-  <div className="entirePage">
   <title > Welcome to my Website!</title>
 
   {/* header*/}
-  <div className="relative flex justify-center items-center w-[100vw]">
-    <h1 className='sm: text-3xl md:text-5xl'>Gabriel Jackson</h1>
+  <div className="relative flex justify-center items-center w-full">
+    <h1 className='sm: text-5xl md:text-6xl bg-linear-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent'>Gabriel Jackson</h1>
   </div>
 
-  {/* body*/}
-
+    {/* body*/}
+    <div className='flex-1 pt-10'>
     <div className="bodyGrid">
       <div className="rounded-3xl bg-green-200/60">
-      <Blob blobTitle={"About me"} blobBody={"Hi, I'm Gabriel Jackson — a Computer Science and Engineering student at UConn with a passion for building clean, responsive, and user-focused web applications."}></Blob>
+      <Blob blobTitle={"About me"} blobBody={"Hi, I'm Gabriel Jackson — a Computer Science and Engineering Graduate from UConn with a passion for building clean, responsive, and user-focused web applications."}></Blob>
       </div>
     <div className="rounded-3xl bg-blue-200/60">
       <Blob blobTitle={"My Journey"} blobBody={"After graduating I found myself struggling to find a job. In response, I set out to learn more, work harder, and create something tangible to show my progress"}></Blob>
     </div>
-    <div className="rounded-3xl bg-yellow-200/70">
-      <Blob blobTitle={"Projects"} blobBody={"UConn Senior Design Project \n Infosys Code Conversion Chatbot \n Portfolio Website"}></Blob>
+    <div className="rounded-3xl bg-yellow-200/70 whitespace-pre-line">
+      <Blob blobTitle={"Projects"} blobBody={"UConn Senior Design Project \n Infosys Code Conversion Chatbot\n Portfolio Website\n"}></Blob>
+    </div>
     </div>
     </div>
 
     {/*Footer */}
 
-    <div className="fixed bottom-0 left-0 w-full bg-gray-200/20 text-center">
-    
+    <div className="flex flex-col w-full bg-gray-200/20 text-center">
       <h2>Contact me:</h2>
       <a href="mailto:gabejackson01@gmail.com">gabejackson01@gmail.com</a>
-      <br></br>
       <a href="https://github.com/gabejacksonisgoodatcoding">Check out my github!</a>
     </div>
   </div>
-  </>
 );
   }
